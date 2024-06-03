47cd080d46032aa7adfc3f681f374dc9a73c7429
RonaldoMunoz <yeifer.munoz@correounivalle.edu.co>
2024-06-02 22:37:24 -0500

    Implementacion deriva

--- app/src/main/scala/taller4/Newton.scala
+++ app/src/main/scala/taller4/Newton.scala
@@ -13,5 +13,19 @@ class Newton {
       case Expo(e1, e2) => s"(${mostrar(e1)} ^ ${mostrar(e2)})"
       case Logaritmo(e1) => s"(lg(${mostrar(e1)}))"
     }
- 
+  def derivar (f:Expr,a:Atomo): Expr = {
+    f match {
+      case Numero(d) => Numero(0)
+      case Atomo(x) => if (x == a.x) Numero(1) else Numero(0)
+      case Suma(e1, e2) => Suma(derivar(e1, a), derivar(e2, a))
+      case Prod(e1, e2) => Suma(Prod(derivar(e1, a), e2), Prod(e1, derivar(e2, a)))
+      case Resta(e1, e2) => Resta(derivar(e1, a), derivar(e2, a))
+      case Div(e1, e2) => Div(Resta(Prod(derivar(e1, a), e2), Prod(e1, derivar(e2, a))), Expo(e2, Numero(2)))
+      case Expo(e1, e2) => Prod(Expo(e1,e2),Prod(Suma(Div(Prod(derivar(e1,a),e2),e1),derivar(e2,a)),Logaritmo(e1)))
+
+     // case Expo(e1,e2) => Prod(Prod(e2,Expo(e1,Resta(e2,Numero(1))),derivar(e1,a)))
+      case Logaritmo(e1) => Div(derivar(e1, a), e1)
+
+    }
+  }
 }
--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -9,14 +9,15 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 
-object Taller4{
+object Taller4 {
 
   def saludo() = "Taller 4"
 
   def main(args: Array[String]): Unit = {
     val objNewton = new Newton()
-    println(Newton())
-    println("Hola Mundo")
+    println(objNewton.mostrar(objNewton.derivar(Expo(Atomo('x'), Numero(2)), Atomo('x'))))
+    println(objNewton.mostrar(objNewton.derivar(Expo(Atomo('x'), Numero(3)), Atomo('x'))))
+    /*    println("Hola Mundo")
     println(
       withWarmer(new Warmer.Default) measure {
         (1 to 100000000).toArray
@@ -24,4 +25,6 @@ object Taller4{
       }
     )
   }
- }
+  */
+  }
+}
