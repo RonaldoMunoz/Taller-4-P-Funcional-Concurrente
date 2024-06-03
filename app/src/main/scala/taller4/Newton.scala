1692d8e9d5225924c5d9dad50babdafc7ff9c775
MaicolRodriguez12 <michael.arana@correounivalle.edu.co>
2024-06-03 11:10:35 -0500

    def limpiar

--- app/src/main/scala/taller4/Newton.scala
+++ app/src/main/scala/taller4/Newton.scala
@@ -23,10 +23,26 @@ class Newton {
     val expr5 = Prod(Div(expr1, expr2), Resta(expr3, expr4))
     val expr6 = Expo(Atomo('x'), Numero(3))
   }
-  println(mostrar(expr1))
-  println(mostrar(expr2))
-  println(mostrar(expr3))
-  println(mostrar(expr4))
-  println(mostrar(expr5))
-  println(mostrar(expr6))
+  def limpiar(expr: Expr): Expr =
+    expr match {
+    case Suma(Numero(0), e) => limpiar(e)
+    case Suma(e, Numero(0)) => limpiar(e)
+    case Prod(Numero(1), e) => limpiar(e)
+    case Prod(e, Numero(1)) => limpiar(e)
+    case Prod(Numero(0), _) => Numero(0)
+    case Prod(_, Numero(0)) => Numero(0)
+    case Resta(e, Numero(0)) => limpiar(e)
+    case Resta(e1, e2) if e1 == e2 => Numero(0)
+    case Div(e, Numero(1)) => limpiar(e)
+    case Expo(e, Numero(1)) => limpiar(e)
+    case Expo(_, Numero(0)) => Numero(1)
+    case Logaritmo(Numero(1)) => Numero(0)
+    case Suma(e1, e2) => Suma(limpiar(e1), limpiar(e2))
+    case Prod(e1, e2) => Prod(limpiar(e1), limpiar(e2))
+    case Resta(e1, e2) => Resta(limpiar(e1), limpiar(e2))
+    case Div(e1, e2) => Div(limpiar(e1), limpiar(e2))
+    case Expo(e1, e2) => Expo(limpiar(e1), limpiar(e2))
+    case Logaritmo(e) => Logaritmo(limpiar(e))
+    case e => e
+  }
 }
